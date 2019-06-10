12d9815c7d524c720146311a0c7bcf41f1ece6a3
Daniel Hongel and hannah melendy <hannah@>
2019-06-10 10:55:35 -0700

    add form structure and logic, intial text, and style buttons.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//business logic
+
+
+
+
+//interface logic
+
+$(function() {
+var favLists = [];
+  ("#favorite").submit(function(event) {
+    var personInput = $("#person").val();
+    var thingInput = $("#thing").val();
+    var placeInput = $("#place").val();
+
+    
+
+
+    event.preventDefault();
+  });
+
+});
