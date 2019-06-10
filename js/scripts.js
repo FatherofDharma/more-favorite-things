ad3aa8fd285f807c2be979cd15f82bbfa670379d
Daniel Hongel and hannah melendy <daniel@>
2019-06-10 11:31:14 -0700

    add logic to show array as list items.

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,14 @@
 $(function() {
 var favLists = [];
   $("#favorite").submit(function(event) {
-    var personInput = $("#person").val();
-    var thingInput = $("#thing").val();
-    var placeInput = $("#place").val();
+    var personInput = $("input#person").val();
+    var thingInput = $("input#thing").val();
+    var placeInput = $("input#place").val();
 
     favLists.push(personInput, thingInput, placeInput);
+    $("#results").show();
+    $("#placeFinal").text(favLists);
     console.log(favLists);
-
     event.preventDefault();
   });
 
