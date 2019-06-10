f5607041a5de0702ac9bd2eaf5b569962e47379a
Daniel Hongel and hannah melendy <hannah@>
2019-06-10 11:03:29 -0700

    add code to put user input into an array.

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,13 @@
 
 $(function() {
 var favLists = [];
-  ("#favorite").submit(function(event) {
+  $("#favorite").submit(function(event) {
     var personInput = $("#person").val();
     var thingInput = $("#thing").val();
     var placeInput = $("#place").val();
 
-    
-
+    favLists.push(personInput, thingInput, placeInput);
+    console.log(favLists);
 
     event.preventDefault();
   });
